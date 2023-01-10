c480acaf8ccec00a6647e5b0eb5bbb881ecf782c
mdpais <mdpais@gmail.com>
2023-01-09 22:56:14 -0400

    Updated questions to objects as per inquirer documentation

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const generateHTML = ({title, description, installation, usage, license, contrib
 ${description}
 
 ##Table of Contents
-    Installation
-    Usage
-    License
-    Contributing
-    Tests
-    Questions
+    [Installation](#installation)
+    [Usage](#usage)
+    [License](#license)
+    [Contributing](#contributing)
+    [Tests](#tests)
+    [Questions](#questions)
 
 ##Installation
 ${installation}
@@ -35,13 +35,66 @@ ${tests}
 If you have any questions regarding this project, you can contact me through my GitHub profile ${github} or email me at ${email}`;
 
 // TODO: Create an array of questions for user input
-const questions = [`What's the title of your project?`, `Enter a description for your project`, `What are the steps required to install your project?`, `What are the instructions to use your project?`, `Which of the following licenses would you like to use?`, `What are your guidelines for developers contributing to your project?`, `Provide examples on how to run tests for your application.`, `What's the link to your GitHub profile?`, `What's your email address?`];
+const questions = [
+    {
+        type: `input`,
+        name: `title`,
+        message: `What's the title of your project?`,
+    },
+    {
+        type: `input`,
+        name: `description`,
+        message: `Enter a description for your project`,
+    },
+    {
+        type: `input`,
+        name: `installation`,
+        message: `What are the steps required to install your project?`,
+    },
+    {
+        type: `input`,
+        name: `usage`,
+        message: `What are the instructions to use your project?`,
+    },
+    {
+        type: `list`,
+        name: `license`,
+        message: `Which of the following licenses would you like to use?`,
+        choices: [`None`, `Apache License 2.0`, `GNU License`, `MIT License`, `BSD License`, `Boost Software License 1.0`, `Creative Commons Zero`, `Mozilla Public License 2.0`, `The Unilicense`],
+    },
+    {
+        type: `input`,
+        name: `contributing`,
+        message: `What are your guidelines for developers contributing to your project?`,
+    },
+    {
+        type: `input`,
+        name: `tests`,
+        message: `Provide examples on how to run tests for your application.`,
+    },
+    {
+        type: `input`,
+        name: `github`,
+        message: `What's the link to your GitHub profile?`,
+    },
+    {
+        type: `input`,
+        name: `email`,
+        message: `What's your email address?`,
+    }];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {
+//     fs.writeFile(`README.md`, generateHTML(), err =>
+//     err? console.log(`An error was encountered while creating the readme file.` : console.log(`Your readme file was successfully created!`)))
+// }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        console.log(answers);
+      });
+}
 
 // Function call to initialize app
 init();
